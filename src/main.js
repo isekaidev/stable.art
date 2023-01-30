e8d9df2d0e73255a252138d079e61c38ce268943
isekaidev <118139897+isekaidev@users.noreply.github.com>
2023-01-30 22:23:15 +0500

    update sentry

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ Sentry.init({
 
   // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring
   // Sentry recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.7,
 });
 
 new Vue({
